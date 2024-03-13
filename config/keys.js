7fc378921e5b8b9ea9334f32b5592ac17bf27e64
VannemreddiHaritha <47218056+VannemreddiHaritha@users.noreply.github.com>
2024-03-13 19:48:52 -0400

    added prod keys

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,5 @@
-module.exports = {
-  googleClientId:
-    "627415888786-ph8q2v5k1316s7f3i21442j2tb662mik.apps.googleusercontent.com",
-  googleClientSecret: "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",
-  mongoURI:
-    "mongodb+srv://harithavannem:nFPwr9NWHV2I9eX7@cluster0.4p2kqjq.mongodb.net/harithadb?retryWrites=true&w=majority&appName=Cluster0",
-  cookieKey: "ftgfjhgkjyuydrsdcjkhkjluujfgsdsxgvjkl",
-};
+if (process.env.NODE_ENV == "production") {
+  module.exports = require("./prod");
+} else {
+  module.exports = require("./dev");
+}
